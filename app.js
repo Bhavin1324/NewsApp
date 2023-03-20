5d309c46e5e161eca463c75ab39ad786a34c978a
bhavin <bjariwala26@gmail.com>
2023-03-20 23:41:01 +0530

    layout changed. image card introduced

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-let container = document.querySelector('.container');
-let loader = document.querySelector('.loader-bg');
+let container = document.querySelector(".main-bucket");
+let loader = document.querySelector(".loader-bg");
 const apiKey = "c0f766c5639a6d789215ce923430af67";
 
-window.addEventListener('load', ()=>{
-    loader.style.display = 'none';
+window.addEventListener("load", () => {
+  loader.style.display = "none";
 });
 
 async function getNews() {
-    const response = await fetch(`https://gnews.io/api/v4/top-headlines?lang=en&country=in&token=${apiKey}`);
-    const { articles } = await response.json();
-    console.log(articles);
-    articles.forEach((article, index) => {
-        container.innerHTML += `<div class="collapse">
-                <button class="collapsible"><b>Breaking news ${index + 1}: </b>${article.title}</button>
-            <div class="content">
-                <p>${article.content} <a href=${article.url}>Read More</a></p>
-                </div>`;
-        let collapse = document.querySelectorAll('.collapsible');
-        collapse.forEach(col => {
-            col.addEventListener('click', function () {
-                col.parentElement.classList.toggle('active');
-                let content = col.nextElementSibling;
-                if (content.style.maxHeight) {
-                    content.style.maxHeight = null;
-                } else {
-                    content.style.maxHeight = content.scrollHeight + "px";
-                }
-            })
-        })
-    });
+  const response = await fetch(
+    `https://gnews.io/api/v4/top-headlines?lang=en&country=in&token=${apiKey}`
+  );
+  const { articles } = await response.json();
+  console.log(articles);
+  articles.forEach((article, index) => {
+    container.innerHTML += `<div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="card mx-auto mb-4 border-0 shadow" style="width: 20rem;">
+                    <img src="${
+                      article.image === null || article.image === ""
+                        ? "./images/blank.jpg"
+                        : article.image
+                    }"
+                        class="card-img-top" alt="image" height="200px" width="200px">
+                    <div class="card-body">
+                        <h5 class="card-title">${article.title}</h5>
+                        <p class="card-text">${article.content}</p>
+                        <div class="mb-2" style="width: fit-content;margin-left: auto"><a href="${
+                          article.url
+                        }" class="link-violet">Read more</a></div>
+                    </div>
+                </div>
+            </div>`;
+  });
 }
 getNews();
